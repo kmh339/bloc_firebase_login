356682f6498c63111c3ecc7dd987e889a80878ff
kmh339 <kmh339@gmail.com>
2019-12-30 13:58:09 +0900

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -7,3 +7,43 @@ import 'package:bloc_firebase_login/home_screen.dart';
 import 'package:bloc_firebase_login/login/login.dart';
 import 'package:bloc_firebase_login/splash_screen.dart';
 import 'package:bloc_firebase_login/simple_bloc_delegate.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  final UserRepository userRepository = UserRepository();
+  runApp(
+    BlocProvider(
+      create: (context) => AuthenticationBloc(
+        userRepository: userRepository,
+      )..add(AppStarted()),
+      child: App(userRepository: userRepository),
+    ),
+  );
+}
+
+class App extends StatelessWidget {
+  final UserRepository _userRepository;
+
+  App({Key key, @required UserRepository userRepository})
+      : assert(userRepository != null),
+        _userRepository = userRepository,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state is Unauthenticated) {
+            return LoginScreen(userRepository: _userRepository);
+          }
+          if (state is Authenticated) {
+            return HomeScreen(name: state.displayName);
+          }
+          return SplashScreen();
+        },
+      ),
+    );
+  }
+}
